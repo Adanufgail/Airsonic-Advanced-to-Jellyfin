7c0750a8fdb3caa0eebaf0c035ece17ec4af29e4
Adanufgail <1276110+Adanufgail@users.noreply.github.com>
2024-01-12 22:44:00 -0700

    Starred items pulled from Airsonic, API auth to Jellyfin set up, Searching Jellyfin for songs kinda works but isn't perfect yet

--- airsonic-advanced-to-jellyfin.py
+++ airsonic-advanced-to-jellyfin.py
@@ -2,8 +2,9 @@
 import configparser
 import json
 import re
-from urllib.request import urlopen
-from urllib.parse import quote_plus as ursafe
+#from urllib.request import urlopen
+import urllib.request
+from urllib.parse import quote as ursafe
 import random
 import string
 import hashlib
@@ -14,22 +15,19 @@ import os
 
 '''
 Airsonic-Advanced-To-Jellyfin
-Version 0.0.1
+Version 0.0.2
 Author: Michael Lubert (Adanufgail@Github)
-Date: 2024-01-11
+Date: 2024-01-12
 '''
 
 '''
 TODO:
 
 Airsonic:
-	Pull all song directories into array
 	Pull Playlists
 
 Jellyfin:
-	Connect To Jellyfin API
-	Confirm how API returns song paths
-	Find song by path?
+	Confirm the same song in Jellyfin and Subsonic
 	Star songs
 	Create playlists and add songs to them
 '''
@@ -72,42 +70,104 @@ def cred_read():
 
 # Generate a random token to use when connecting to Airsonic
 
-def token_gen():
-	global salt
-	global token
-	salt='%030x' % random.randrange(16**30)
-	cram=air["pass"]+salt
-	token=hashlib.md5(cram.encode()).hexdigest()
+def air_token_gen():
+	global air_salt
+	global air_token
+	air_salt='%030x' % random.randrange(16**30)
+	cram=air["pass"]+air_salt
+	air_token=hashlib.md5(cram.encode()).hexdigest()
 
 # Use Airsonic REST API to run "command"
 
 def airsonic_rest(page,args):
-	url = air["url"]+"/rest/"+page+"?v=1.15.0&c=python&username="+air["user"]+"&u="+air["user"]+"&s="+salt+"&t="+token+args
-	print(url)
-	data = urlopen(url)
+	url = air["url"]+"/rest/"+page+"?v=1.15.0&c=python&username="+air["user"]+"&u="+air["user"]+"&s="+air_salt+"&t="+air_token+args
+	data = urllib.request.urlopen(url)
 	usable=ET.fromstring(data.read())
 	return usable
+
+def airsonic_starred():
+	global air_stars
+
+	# Connect to Airsonic and pull all starred songs
+	air_token_gen() #this only needs to be called once per session
+	astar_raw=airsonic_rest("getStarred2","")
+	astar=astar_raw[0]
+	air_stars = {}
+
+	# Parse each entry for the Artist/Album/Title/Track
+	for song in astar:
+		sid=song.attrib["id"]
+		air_stars[sid]={}
+		air_stars[sid]["title"]=song.get("title","")
+		air_stars[sid]["artist"]=song.get("artist","")
+		air_stars[sid]["album"]=song.get("album","")
+		air_stars[sid]["track"]=song.get("track","")
+		
+
+def jellyfin_rest(page,args="NULL"):
+	# Log In with Username Code
+	if page == "Users/AuthenticateByName":
+		login = { "Username" : jel["user"], "Pw" : jel["pass"] }
+		login = json.dumps(login)
+		login = str(login)
+		login = login.encode('utf-8')
+		url = jel["url"]+"/"+page
+		hdr = { 
+			"Content-Type" : "application/json",
+			"accept" : "application/json",
+         "x-emby-authorization":'MediaBrowser Client="Jellyfin CLI",Device="Jellyfin-CLI", DeviceId="None", Version="10.8.0"'
+		}
+		req = urllib.request.Request(url,headers=hdr,data=login,method="POST")
+		data = urllib.request.urlopen(req)
+		usable=data.read()
+		usable = json.loads(usable)
+	# Normal Page Loading
+	else:
+		if(args=="NULL"):
+			url = jel["url"]+"/"+page
+		else:
+			url = jel["url"]+"/"+page+"?"+args
+		hdr = { 
+			"accept" : "application/json",
+         "x-emby-authorization":'MediaBrowser Client="Jellyfin CLI",Device="Jellyfin-CLI", DeviceId="None", Version="10.8.0"',
+			"accept" : "application/json",
+			"x-mediabrowser-token": jel_token
+		}
+		req = urllib.request.Request(url,headers=hdr)
+		data = urllib.request.urlopen(req)
+		usable=json.loads(data.read())
+	return usable
+
+def jellyfin_gettoken():
+	global jel_token
+	global jel_uid
+	
+	# Log In and get AccessToken
+	jel_token = jellyfin_rest("Users/AuthenticateByName","")["AccessToken"]
+	jel_uid = jellyfin_rest("Users/AuthenticateByName","")["User"]["Id"]
+	
+
+def jellyfin_findsong(title,band,album="",track=""):
+	args="recursive=true&includeItemTypes=Audio&userId="+jel_uid+"&artists="+ursafe(band)+"&searchTerm="+ursafe(title)
+	# Find a song by Title, Artist, and Album (if available). Will then cross reference Track Number (if available)
+	if(album!=""):
+		args=args+"&albums="+ursafe(album)
+	return jellyfin_rest("Users/"+jel_uid+"/Items",args)
+	
 
 
 '''
 MAIN PROGRAM LOGIC
 '''
 
-# Get Config File
+def main():
+	# Get Config File
 
-cred_exist(cred_filename)
-cred_read()
+	cred_exist(cred_filename)
+	cred_read()
+	
+	airsonic_starred()
+	jellyfin_gettoken()
+	print(jellyfin_findsong("PDA","Interpol","Turn on the Bright Lights",4))
 
-
-# Connect to Airsonic and pull all starred songs
-
-token_gen() #this only needs to be called once per session
-astar_raw=airsonic_rest("getStarred2","")
-astar=astar_raw[0]
-
-# Get file path for each song
-# This is the raw path, based on the Airsonic configured directory (ie "ARTIST/Album/Song.mp3" and not /media/music/ARTIST/Album/Song.mp3"
-
-#for stared in astar: #Add this back in for all songs, debug below for one to get formatting right
-song_raw=airsonic_rest("getSong","&id="+astar[0].attrib["id"])
-print(song_raw[0].attrib["path"]);
+main()
